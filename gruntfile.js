7ba8c28874eaa347789c306529317b9a09e01e0e
Jacq <jacq1212@hotmail.com>
2014-09-23 12:08:54 +0200

    Added both css icons

--- gruntfile.js
+++ gruntfile.js
@@ -1,174 +1,192 @@
 module.exports = function (grunt) {
-	"use strict";
+    "use strict";
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		pkg: grunt.file.readJSON('bower.json'),
-        icons : grunt.option('icons') || 'dark',
-		language: grunt.option('lang') || 'en',
+        pkg: grunt.file.readJSON('bower.json'),
+        icons: grunt.option('icons') || 'dark',
+        language: grunt.option('lang') || 'en',
 
-		meta: {
-			banner: '/**\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				' * <%= pkg.homepage %>\n' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
-				' Licensed <%= pkg.license %>\n */\n'
-		},
+        meta: {
+            banner: '/**\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+                ' * <%= pkg.homepage %>\n' +
+                ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+                ' Licensed <%= pkg.license %>\n */\n'
+        },
 
-		build_dir: 'build',
+        build_dir: 'build',
+        lib_files: {
 
-		lib_files: {
+            core: [
+            'src/growl.js',
+            'src/growlDirective.js',
+            'src/growlFactory.js',
+            'src/growlMessageService.js'
+            ],
+            cssdark: [
+            'src/growl.css',
+            'src/growl.dark.css'
+            ],
+            csswhite: [
+            'src/growl.css',
+            'src/growl.white.css'
+            ],
+            test: ['test/**/*.js']
+        },
+        watch: {
 
-			core: [
-				'src/growl.js',
-				'src/growlDirective.js',
-				'src/growlFactory.js',
-				'src/growlMessageService.js'
-			],
-			css:  [
-				'src/growl.css',
-				'src/growl.<%= icons %>.css',
-			],
-			test: ['test/**/*.js']
-		},
+            scripts: {
+                files: ['gruntfile.js', '<%= lib_files.core %>', '<%= lib_files.test %>'],
+                tasks: ['jshint:all', 'karma:unit']
+            },
 
-		watch: {
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: ['src/**/*.*'],
+                tasks: ['jshint', 'karma:unit']
+            }
+        },
 
-			scripts: {
-				files: ['gruntfile.js', '<%= lib_files.core %>', '<%= lib_files.test %>'],
-				tasks: ['jshint:all', 'karma:unit']
-			},
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
 
-			livereload: {
-				options: {
-					livereload: true
-				},
-				files: ['src/**/*.*'],
-				tasks: ['jshint', 'karma:unit']
-			}
-		},
+            all: ['gruntfile.js', '<%= lib_files.core %>', '<%= lib_files.test %>'],
 
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
+            core: {
+                files: {
+                    src: ['<%= lib_files.core %>']
+                }
+            },
 
-			all: ['gruntfile.js', '<%= lib_files.core %>', '<%= lib_files.test %>'],
+            test: {
+                files: {
+                    src: ['<%= lib_files.test %>']
+                }
+            }
+        },
 
-			core: {
-				files: {
-					src: ['<%= lib_files.core %>']
-				}
-			},
+        concat: {
+            banner: {
+                options: {
+                    banner: '<%= meta.banner %>'
+                },
+                src: '<%= concat.core.dest %>',
+                dest: '<%= concat.core.dest %>',
+            },
 
-			test: {
-				files: {
-					src: ['<%= lib_files.test %>']
-				}
-			}
-		},
+            core: {
+                src: ['<%= lib_files.core %>'],
+                dest: '<%= build_dir %>/angular-growl.js'
+            },
+            cssdark: {
+                options: {
+                    banner: '<%= meta.banner %>'
+                },
+                src: ['<%= lib_files.cssdark %>'],
+                dest: '<%= build_dir %>/angular-growl-dark.css'
+            },
+            csswhite: {
+                options: {
+                    banner: '<%= meta.banner %>'
+                },
+                src: ['<%= lib_files.csswhite %>'],
+                dest: '<%= build_dir %>/angular-growl-white.css'
+            }
+        },
+        cssmin: {
+            coredark: {
+                files: {
+                    'build/angular-growl.dark.min.css': '<%= lib_files.cssdark %>'
+                },
+                options: {
+                    'banner': '<%= meta.banner %>',
+                    'report': 'gzip'
+                }
+            },
+            corewhite: {
+                files: {
+                    'build/angular-growl.white.min.css': '<%= lib_files.csswhite %>'
+                },
+                options: {
+                    'banner': '<%= meta.banner %>',
+                    'report': 'gzip'
+                }
+            }
+        },
 
-		concat: {
-			banner: {
-				options: {
-					banner: '<%= meta.banner %>'
-				},
-				src: '<%= concat.core.dest %>',
-				dest: '<%= concat.core.dest %>',
-			},
+        uglify: {
+            core: {
+                files: {
+                    '<%= build_dir %>/angular-growl.min.js': '<%= concat.core.dest %>'
+                },
+                options: {
+                    banner: '<%= meta.banner %>',
+                    report: 'gzip'
+                }
+            }
+        },
 
-			core: {
-				src: ['<%= lib_files.core %>'],
-				dest: '<%= build_dir %>/angular-growl.js'
-			},
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        },
 
-			css: {
-				options: {
-					banner: '<%= meta.banner %>'
-				},
-				src: ['<%= lib_files.css %>'],
-				dest: '<%= build_dir %>/angular-growl.css'
-			}
-		},
-
-		cssmin: {
-			core: {
-				files: {
-					'build/angular-growl.min.css': '<%= lib_files.css %>'
-				},
-				options: {
-					'banner': '<%= meta.banner %>',
-					'report': 'gzip'
-				}
-			}
-		},
-
-		uglify: {
-			core: {
-				files: {
-					'<%= build_dir %>/angular-growl.min.js': '<%= concat.core.dest %>'
-				},
-				options: {
-					banner: '<%= meta.banner %>',
-					report: 'gzip'
-				}
-			}
-		},
-
-		karma: {
-			unit: {
-				configFile: 'karma.conf.js',
-				singleRun: true
-			}
-		},
-
-		ngmin: {
-			core: {
-				src: '<%= concat.core.dest %>',
-				dest: '<%= concat.core.dest %>'
-			}
-		},
-		push: {
-			options: {
-				files: ['package.json', 'bower.json'],
-				add: true,
-				addFiles: ['.'], // '.' for all files except ingored files in .gitignore
-				commit: true,
-				commitMessage: 'Release v%VERSION%',
-				commitFiles: ['package.json', 'bower.json', 'build/angular-growl.js', 'build/angular-growl.min.js', 'build/angular-growl.min.css', 'README.md'], // '-a' for all files
-				createTag: true,
-				tagName: 'v%VERSION%',
-				tagMessage: 'Version %VERSION%',
-				push: true,
-				pushTo: 'origin',
-				npm: true,
-				npmTag: 'Release v%VERSION%',
-				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
-			}
-		}
-	});
+        ngmin: {
+            core: {
+                src: '<%= concat.core.dest %>',
+                dest: '<%= concat.core.dest %>'
+            }
+        },
+        push: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                add: true,
+                addFiles: ['.'], // '.' for all files except ingored files in .gitignore
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json', 'bower.json', 'build/angular-growl.js', 'build/angular-growl.min.js', 'build/angular-growl.dark.min.css', 'build/angular-growl.white.min.css', 'README.md'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                npm: true,
+                npmTag: 'Release v%VERSION%',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+            }
+        }
+    });
 
 
-	grunt.registerTask('default', ['jshint:all', 'karma']);
-	grunt.registerTask('test', ['karma']);
+    grunt.registerTask('default', ['jshint:all', 'karma']);
+    grunt.registerTask('test', ['karma']);
 
-	grunt.registerTask('build', [
-		'jshint:all',
-		'karma',
-		'build:core'
-	]);
+    grunt.registerTask('build', [
+      'jshint:all',
+      'karma',
+      'build:core'
+     ]);
 
-	grunt.registerTask('build:core', [
-		'concat:core',
-		'concat:css',
-		'ngmin:core',
-		'concat:banner',
-		'uglify:core',
-		'cssmin:core'
-	]);
+    grunt.registerTask('build:core', [
+      'concat:core',
+      'concat:cssdark',
+      'concat:csswhite',
+      'ngmin:core',
+      'concat:banner',
+      'uglify:core',
+      'cssmin:coredark',
+      'cssmin:corewhite',
+     ]);
 
-	// For development purpose.
-	grunt.registerTask('dev', ['jshint', 'karma:unit', 'watch:livereload']);
+    // For development purpose.
+    grunt.registerTask('dev', ['jshint', 'karma:unit', 'watch:livereload']);
 
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 };
